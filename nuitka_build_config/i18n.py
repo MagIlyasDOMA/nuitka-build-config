34a2aab0bb12913db2ff0c25ee9c0d1ee840d04f
Маг Ильяс DOMA <magilyas.doma.09@list.ru>
2026-04-08 22:38:29 +0300

    Load .mo translations and add locales
    Change i18n to load compiled .mo files via pathlib and GNUTranslations (fallback to 'en_US' if locale not found). Add compiled nbc.mo files for en_US and ru_RU under LC_MESSAGES and move existing .po files into the LC_MESSAGES directories. Also update .gitignore to ignore structure.txt, test.py, and *.pyc.

--- nuitka_build_config/i18n.py
+++ nuitka_build_config/i18n.py
@@ -1,7 +1,11 @@
 import gettext as i18n_lib, locale
+from pathlib import Path
+
 
 locale.setlocale(locale.LC_ALL, '')
 
-lang = i18n_lib.translation('nbc', 'locale', [locale.getlocale()[0]])
+mo_path = Path(__file__).parent / 'locale' / (locale.getlocale()[0] or 'en_US') / 'nbc.mo'
 
+with open(mo_path, 'rb') as file:
+    lang = i18n_lib.GNUTranslations(file)
 gettext = lang.gettext
