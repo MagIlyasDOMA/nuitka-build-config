865756364ebb2826aea6048f2d81b3e6d6f7e5b4
Маг Ильяс DOMA <magilyas.doma.09@list.ru>
2026-04-09 23:19:15 +0300

    Update i18n.py

--- nuitka_build_config/i18n.py
+++ nuitka_build_config/i18n.py
@@ -1,10 +1,11 @@
-import gettext as i18n_lib, locale
+import locale
 from pathlib import Path
+from locale_plus import Internationalizator
 
 
 locale.setlocale(locale.LC_ALL, '')
 
-mo_path = Path(__file__).parent / 'locale' / (locale.getlocale()[0] or 'en_US') / 'nbc.mo'
+internationalizator = Internationalizator()
 
 with open(mo_path, 'rb') as file:
     lang = i18n_lib.GNUTranslations(file)
