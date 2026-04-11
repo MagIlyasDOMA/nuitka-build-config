22f4bcbd3ce865aabcdee4c73fcd7e2b41d1e543
Маг Ильяс DOMA <magilyas.doma.09@list.ru>
2026-04-11 21:19:32 +0300

    2026-04-11 21:19:32

--- nuitka_build_config/__init__.py
+++ nuitka_build_config/__init__.py
@@ -1,10 +1,21 @@
 import os
 from pathlib import Path
 from pathlike_typing import PathLike
+from .descriptors import CachedProperty, UncacheProperty
+from .models import NuitkaConfig
 
 
 class NuitkaBuilder:
-    def __init__(self, config_path: PathLike):
+    config_path: Path = UncacheProperty('config', 'command')
+
+    def __init__(self, config_path: PathLike = 'nbc-config.yaml'):
         self.config_path = Path(config_path)
-        
+
+    @CachedProperty
+    def config(self) -> NuitkaConfig:
+        return NuitkaConfig.from_yaml_file(self.config_path)
+
+    @CachedProperty
+    def command(self) -> str:
+        config = self.config
 
