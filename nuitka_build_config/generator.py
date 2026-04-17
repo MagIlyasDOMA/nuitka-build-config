96f7d58868757534dc38ff041e08f2ebd35c131c
Маг Ильяс DOMA <magilyas.doma.09@list.ru>
2026-04-17 23:16:06 +0300

    Update generator.py

--- nuitka_build_config/generator.py
+++ nuitka_build_config/generator.py
@@ -1,37 +1,12 @@
 import argparse
+from typing import Self
 from pathlib import Path
 from .i18n import gettext
 from .models import NuitkaConfig
 from .typings.models import NuitkaConfigDict
 
 
-class GroupsDictMixin(argparse.ArgumentParser):
-    groups: dict
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.groups = dict()
-
-    def add_argument_group(
-        self,
-        title=None,
-        description=None,
-        name: str = ...,
-        **kwargs
-    ) -> argparse._ArgumentGroup:
-        group = super().add_argument_group(
-            title, description, **kwargs
-        )
-        self.groups[name] = group
-        return group
-
-    def add_mutually_exclusive_group(self, *, required=False, name: str = ...) -> argparse._MutuallyExclusiveGroup:
-        group = super().add_mutually_exclusive_group(required=required)
-        self.groups[name] = group
-        return group
-
-
-class NuitkaParser(GroupsDictMixin):
+class NuitkaParser(argparse.ArgumentParser):
     def _add_config_root_arguments(self):
         self.add_argument('--mode', dest='type',
                           help=gettext("Compilation mode: 'accelerated' (with Python dependency), 'standalone' (folder with exe), "
@@ -40,7 +15,7 @@ class NuitkaParser(GroupsDictMixin):
         self.add_argument('--run', dest='run', action='store_true',
                           help=gettext("Run the compiled binary immediately after compilation (--run)"))
 
-        follow_imports_group = self.add_mutually_exclusive_group(name='follow_imports')
+        follow_imports_group = self.add_mutually_exclusive_group()
         follow_imports_group.add_argument('--follow-imports', action='store_const', const=True, dest='follow_imports',
                                           help=gettext("Follow all imported modules. In standalone mode defaults to True"))
         follow_imports_group.add_argument('--nofollow-imports', action='store_const', const=False, dest='follow_imports',
@@ -56,7 +31,7 @@ class NuitkaParser(GroupsDictMixin):
                           help=gettext("List of plugins to disable"))
         self.add_argument('--main', type=Path, dest='main',
                           help=gettext("Main file to compile (alternative to positional argument --main)"))
-        self.add_argument('main', type=Path, dest='main', nargs='?',
+        self.add_argument('main_pos', type=Path, metavar='MAIN', nargs='?',
                           help=gettext("Main file to compile (positional argument)"))
         self.add_argument('--python-flag', action='append', dest='python_flags',
                           help=gettext("Python flags: '-S' (no_site), '-O' (no_asserts), 'no_warnings', 'no_docstrings', '-u' (unbuffered)"))
@@ -81,54 +56,54 @@ class NuitkaParser(GroupsDictMixin):
         self.set_defaults(verbosity='info', type='accelerated')
 
     def _add_includes_arguments(self):
-        includes_group = self.add_argument_group("Includes", name='include',
-            description=gettext("Settings for including additional modules, packages and data files"))
-        includes_group.add_argument('--include-package', action='append', dest='includes__packages',
+        includes_group = self.add_argument_group("Includes",
+                                                 description=gettext("Settings for including additional modules, packages and data files"))
+        includes_group.add_argument('--include-package', action='append', dest='include__packages',
                                     help=gettext("List of packages to include in the build (--include-package)"))
-        includes_group.add_argument('--include-module', action='append', dest='includes__modules',
+        includes_group.add_argument('--include-module', action='append', dest='include__modules',
                                     help=gettext("List of modules to include in the build (--include-module)"))
-        includes_group.add_argument('--include-package-data', action='append', dest='includes__package_data',
+        includes_group.add_argument('--include-package-data', action='append', dest='include__package_data',
                                     help=gettext("Packages whose data files should be included (--include-package-data)"))
-        includes_group.add_argument('--include-data-files', action='append', dest='includes__files',
+        includes_group.add_argument('--include-data-files', action='append', dest='include__files',
                                     help=gettext("File patterns to include (--include-data-files)"))
-        includes_group.add_argument('--include-data-dir', action='append', dest='includes__directories',
+        includes_group.add_argument('--include-data-dir', action='append', dest='include__directories',
                                     help=gettext("Data directories to include (--include-data-dir)"))
-        includes_group.add_argument('--noinclude-data-files', action='append', dest='includes__noinclude_data_files',
+        includes_group.add_argument('--noinclude-data-files', action='append', dest='include__noinclude_data_files',
                                     help=gettext("File patterns to exclude from data (--noinclude-data-files)"))
 
     def _add_windows_arguments(self):
-        windows_group = self.add_argument_group("Windows parameters", name='windows_params')
-        windows__icon = windows_group.add_mutually_exclusive_group()
-        windows__icon.add_argument('--windows-icon-from-ico', dest='windows__icon', type=Path,
+        windows_group = self.add_argument_group("Windows parameters")
+        windows_params__icon = windows_group.add_mutually_exclusive_group()
+        windows_params__icon.add_argument('--windows-icon-from-ico', dest='windows_params__icon', type=Path,
                                    help=gettext("Path to icon file (ICO format)"))
-        windows__icon.add_argument('--windows-icon-from-exe', dest='windows__icon', type=Path,
+        windows_params__icon.add_argument('--windows-icon-from-exe', dest='windows_params__icon', type=Path,
                                    help=gettext("Extract icon from existing EXE file"))
-        windows_group.add_argument('--windows-console-mode', dest='windows__console_mode',
+        windows_group.add_argument('--windows-console-mode', dest='windows_params__console_mode',
                                    choices=['disable', 'force'],
                                    help=gettext("Windows console mode: 'force' - create a console window (if not started from console), "
                                                 "'disable' - do not create a console (--windows-console-mode)"))
-        windows_group.add_argument('--windows-uac-admin', action='store_true', dest='windows__uac_admin',
+        windows_group.add_argument('--windows-uac-admin', action='store_true', dest='windows_params__uac_admin',
                                    help=gettext("Request administrator privileges via UAC on launch (--windows-uac-admin)"))
-        windows_group.add_argument('--windows-uac-uiaccess', action='store_true', dest='windows__uac_uiaccess',
+        windows_group.add_argument('--windows-uac-uiaccess', action='store_true', dest='windows_params__uac_uiaccess',
                                    help=gettext("Request UAC to launch only from specific folders and remote access (--windows-uac-uiaccess)"))
 
     def _add_macos_arguments(self):
-        macos_group = self.add_argument_group("MacOS parameters", name='macos_params')
-        macos_group.add_argument('--macos-app-icon', dest='macos__icon', type=Path,
+        macos_group = self.add_argument_group("MacOS parameters")
+        macos_group.add_argument('--macos-app-icon', dest='macos_params__icon', type=Path,
                                  help=gettext("Path to icon file for macOS .app bundle (--macos-app-icon)"))
-        macos_group.add_argument('--macos-create-app-bundle', dest='macos__create_app_bundle', action='store_true',
+        macos_group.add_argument('--macos-create-app-bundle', dest='macos_params__create_app_bundle', action='store_true',
                                  help=gettext("Create a .app bundle instead of a regular binary. Enables standalone mode (--macos-create-app-bundle)"))
-        macos_group.add_argument('--macos-signed-app-name', dest='macos__signed_app_name',
+        macos_group.add_argument('--macos-signed-app-name', dest='macos_params__signed_app_name',
                                  help=gettext("Application name for macOS signing in 'com.YourCompany.AppName' format. "
                                               "Globally unique identifier for accessing protected APIs (--macos-signed-app-name)"))
 
     def _add_linux_arguments(self):
-        linux_group = self.add_argument_group("Linux parameters", name='linux_params')
-        linux_group.add_argument('--linux-icon', dest='linux__icon', type=Path,
+        linux_group = self.add_argument_group("Linux parameters")
+        linux_group.add_argument('--linux-icon', dest='linux_params__icon', type=Path,
                                  help=gettext("Path to icon file for Linux (--linux-icon)"))
 
     def _add_version_info_arguments(self):
-        version_info_group = self.add_argument_group("Version info", name='version_info')
+        version_info_group = self.add_argument_group("Version info")
         version_info_group.add_argument('--company-name', dest='version_info__company_name',
                                         help=gettext("Company name in version info (--company-name)"))
         version_info_group.add_argument('--product-name', dest='version_info__product_name',
@@ -139,13 +114,13 @@ class NuitkaParser(GroupsDictMixin):
                                         help=gettext("Copyright text (--copyright)"))
 
     def _add_actions_arguments(self):
-        actions_group = self.add_argument_group("Actions", name='actions')
+        actions_group = self.add_argument_group("Actions")
         actions_group.add_argument('--add-pre-compile-action', '-b',
                                    dest='pre_compile_actions', help=gettext("Commands executed before compilation"))
         actions_group.add_argument('--add-post-compile-action', '-p',
                                    dest='post_compile_actions', help=gettext("Commands executed after compilation"))
 
-    def add_arguments(self):
+    def add_arguments(self) -> Self:
         self._add_config_root_arguments()
         self._add_includes_arguments()
         self._add_windows_arguments()
@@ -153,10 +128,21 @@ class NuitkaParser(GroupsDictMixin):
         self._add_linux_arguments()
         self._add_version_info_arguments()
         self._add_actions_arguments()
+        return self
+
+    def _validate_main(self, dct: dict, /) -> str:
+        main_opt = dct.get('main', None)
+        main_pos = dct.pop('main_pos') if 'main_pos' in dct else None
+        if not main_pos and not main_opt: self.error(gettext("'main' option is required"))
+        elif main_pos and main_opt: self.error(gettext("Use either just --main or just the positional argument"))
+        output = main_pos or main_opt
+        dct['main'] = output
+        return output
 
     def parse_to_dict(self, args=None, add_nones: bool = False) -> NuitkaConfigDict:
         args, extra = self.parse_known_args(args)
         raw_dict = vars(args)
+        self._validate_main(raw_dict)
         raw_dict['extra_flags'] = extra
         output = dict()
         for key, value in raw_dict.items():
@@ -174,6 +160,7 @@ class NuitkaParser(GroupsDictMixin):
 
 def main():
     parser = NuitkaParser()
+    parser.add_arguments()
     args = parser.parse_to_object()
 
 
