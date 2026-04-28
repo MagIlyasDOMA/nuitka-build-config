6d36f92fae4ea0dfc953d0dd5c68d91e9c636f7e
Маг Ильяс DOMA <magilyas.doma.09@list.ru>
2026-04-28 12:46:04 +0300

    Move MainParser to new module and bump version
    Extract MainParser (and NBCSubcommandsAction) into nuitka_build_config/mainparser.py, removing direct argparse_help_i18n/SubcommandsParser imports from __init__.py and importing MainParser instead. Update main() to instantiate MainParser with the package version. Bump package version to 0.1.6 in pyproject.toml and __init__.py.

--- nuitka_build_config/__init__.py
+++ nuitka_build_config/__init__.py
@@ -1,9 +1,8 @@
 import sys
-from argparse_help_i18n import HelpI18nMixin
-from scparser import SubcommandsParser
 from .builder import *
 from .builder import main as builder_main
 from .i18n import gettext
+from .mainparser import MainParser
 from .models import NuitkaConfig
 from .generator import *
 from .generator import main as generator_main
@@ -13,17 +12,11 @@ from .typings.models import NuitkaConfigDict
 __all__ = ['NuitkaBuilder', 'NuitkaConfig', 'NuitkaConfigDict', 'main', '__version__',
            'NuitkaGenerator', 'GeneratorParser', 'GeneratorArgs', 'generator_main',
            'BuildRunOutput', 'BuilderParser', 'builder_main']
-__version__ = '0.1.5'
-
-
-class MainParser(SubcommandsParser, HelpI18nMixin):
-    def __init__(self, **kwargs):
-        SubcommandsParser.__init__(self, **kwargs)
-        HelpI18nMixin.__init__(self, version=__version__)
+__version__ = '0.1.6'
 
 
 def main():
-    parser = MainParser()
+    parser = MainParser(version=__version__)
     subparsers = parser.add_subparsers(dest='command')
     subparsers.add_parser('build', parser=BuilderParser, help=gettext("Build a Nuitka project"))
     subparsers.add_parser('generate', parser=GeneratorParser, help=gettext("Generate a Nuitka config file"))
---
+++ nuitka_build_config/mainparser.py
@@ -0,0 +1,17 @@
+from argparse_help_i18n import HelpI18nMixin
+from scparser import *
+
+__all__ = ['MainParser', 'NBCSubcommandsAction']
+
+
+class NBCSubcommandsAction(SubcommandsAction):
+    def add_parser(self, name: str, *, parser: ParserType = None, **kwargs):
+        kwargs.setdefault('add_version', False)
+        return super().add_parser(name, parser=parser, **kwargs)
+
+
+class MainParser(SubcommandsParser, HelpI18nMixin):
+    def add_subparsers(self, **kwargs):
+        kwargs.setdefault('action', 'nbc_subcmds')
+        self.register('action', 'nbc_subcmd', SubcommandsAction)
+        return super().add_subparsers(**kwargs)
